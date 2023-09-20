0b1fa51c60cf175a59943a11fc79f95b8e4e4bf3
zosborn1 <96489409+zosborn1@users.noreply.github.com>
2023-09-19 21:01:01 -0500

    testing second method

--- numericError.c
+++ numericError.c
@@ -5,8 +5,7 @@
 *  Can happen during calculation errors. Since the Integers we are using are 32-bits, overflow happens when 
 *   our Integer gets larger then (2^32)+1 [4294967297] or -(2^32)-1 [-4294967297]. 
 *       - For addition: 
-*           x + y > 4294967290 => buffer overflow 
-*               so if 4294967290 - x > y => no buffer overflow. 
+*           x + y > 4294967290 => buffer overflow  
 *       - For subtraction: 
 *           x - y < -4294967297 => buffer overflow
 */
@@ -14,29 +13,33 @@ int UpperOverFlow = 4294967295;
 
 int add(int x, int y) {
     int result = x + y;
-    if ((result < x) || (result < y)) {
-        printf("\n******Error: Overflow with addition******");
+    if ((x > 0 && y > 0 && result < 0) || (x < 0 && y < 0 && result > 0)) {
+        printf("\n\n******Error: Overflow with addition******");
         return -1;
     }
     return result;
-    // if ((x < UpperOverFlow) && (y < (UpperOverFlow - x))) {
-    //     return x + y;
-    // }
-    // else {
-    //     printf("\nError: Overflow with Add()");
-    //     return -1;
-    // }
+}
+
+int subtract(int x, int y) {
+    int result = x - y;
+    if ((result > x) || (result < -4294967296)) {
+        printf("\n\n******Error: Overflow with subtration******");
+        return -1;
+    }
+    return result;
 }
 
 int main() {
     //Addition - should pass 
     int x = 5;
     int y = 15;
-    printf("\n1st addition test: %i", add(x,y));
+    printf("\nPassing test, 5 + 15 = %i", add(x,y));
 
-    //Addition - Should have overflow, testing large x value
+    //Addition - Should have overflow
     int a = 4294967290;
     int b = 10; 
-    printf("\n2nd addition test: %i", add(a,b));
+    printf("\nFailing test, 4294967290 + 10 != %i", add(a,b));
     printf("\n");
+
+    /************************************************/
 }

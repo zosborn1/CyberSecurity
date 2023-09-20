45c8bad5e56dd40dd7e4d3f83634a7427b9543e3
zosborn1 <96489409+zosborn1@users.noreply.github.com>
2023-09-20 11:19:32 -0500

    pushing multiply tests

--- numericError.c
+++ numericError.c
@@ -6,19 +6,23 @@
 *   our Integer gets larger then 2,147,483,647 or smaller then -2,147,483,648. 
 *       - For addition: 
 *             Checking for overflow can come from making sure:
-*                   - the result isnt 0
+*                   - the result isnt 0, when x != (-y)
 *                   - the result is larger then one input. 
 *       - For subtraction: 
 *             Checking for overflow can come from making sure:
-*                   - the result isnt 0
-*                   - result is not larger then the first input
+*                   - the result isnt 0, when x != (-y)
+*                   - result is not larger then the first input, unless one input is negative.
 *       - For Multiplication: 
 *             Checking for overflow can come from making sure:
-*                   - the result isnt 0
-*                   - result is not larger then the first input
+*                   - The inputs do not equal 0
+*                   - the reverce of the multiplication cant happen
+*                   - if both are positive number => result will be positive 
+*                   - if one is a negative number => result will be negative
+*       - For division: 
+*             Since the numbers are intergers and not floats, there will be no overflows. Only check would be that y is not 0. 
 */
 
-/*Constents, headers for functions, and helper functions.*/
+/******Constents, headers for functions, and helper functions.******/
 int MAX_INT = 2147483647;
 
 //So functions can use eachother
@@ -32,7 +36,7 @@ void printErrorStatement(char* functionName) {
     printf("\n\n******Error: Overflow with %s******", functionName);
     return;
 }
-/**************************************/
+/********************************************************************/
 
 int add(int x, int y) {
     //Check to make sure y is possitive, if not, send to subtract
@@ -44,10 +48,11 @@ int add(int x, int y) {
     //Since y is positive the addition will take place
     int result = x + y;
     //Throw error 
-    if ((result < x) || (result == 0 && (x != (-y)))) {
-        char str[] = "Addition";
-        printErrorStatement(str);
-        return -1;
+    if ((result < x) 
+        || (result == 0 && (x != (-y)))) {
+            char str[] = "Addition";
+            printErrorStatement(str);
+            return -1;
     }
     return result;
 }
@@ -63,17 +68,24 @@ int subtract(int x, int y) {
     //Since y is possitive the subtraction will take place
     int result = x - y;
     //Throw error 
-    if ((result > x) || (result == 0)) {
-        char str[] = "Subtraction";
-        printErrorStatement(str);
-        return -1;
+    if ((result > x) 
+        || (result == 0 && (x != (-y)))) {
+            char str[] = "Subtraction";
+            printErrorStatement(str);
+            return -1;
     }
     return result;
 }
 
 int multiply(int x, int y) {
     int result = x * y;
-    if (x == 0 || y == 0 || (x != result / y)) {
+    if (x == 0 
+        || y == 0 
+        || (x != result / y) 
+        || (x < 0 && y > 0 && result > 0)
+        || (x > 0 && y < 0 && result > 0)
+        || (x > 0 && y > 0 && result < 0)) 
+        {
         char str[] = "Multiply";
         printErrorStatement(str);
         return -1;
@@ -90,8 +102,10 @@ int divide(int x, int y) {
     }
     //Check to make sure y is not a decimal, with decimals the end result could overflow
     if (y < 1 && y > 0) {
-
+        y = 1/y;
+        return multiply(x,y);
     }
+    return result;
 }
 
 int main() {
@@ -136,9 +150,9 @@ int main() {
     y = 1; 
     printf("\n%i + %i = %i", x, y, add(x,y));
 
-    printf("\n\n-------------------Subtraction----------------------");
-
     /************************************************/
+
+     printf("\n\n-------------------Subtraction----------------------");
 
     //Subtract w/ positive & positive - should pass
     int a = 5;
@@ -166,8 +180,29 @@ int main() {
     b = 2147483647;
     printf("\n%i - %i = %i", a, b, subtract(a,b));
 
-    printf("\n-----------------------------------------\n");
+    /************************************************/
 
-     /************************************************/
+     printf("\n\n-------------------Multiply----------------------");
+
+    //Multiply with small numbers - should pass
+    int i = 5;
+    int j = 15;
+    printf("\n%i * %i = %i", i, j, multiply(i,j));
+    
+    //Subtract w/ positive larger numbers - Should have overflow
+    i = 0;
+    j = 0;
+    i = 47000;
+    j = 47000;
+    printf("\n%i * %i = %i", i, j, multiply(i,j));
+
+    //Subtract w/ positive larger numbers - Should have overflow
+    i = 0;
+    j = 0;
+    i = -500;
+    j = 47000;
+    printf("\n%i * %i = %i", i, j, multiply(i,j));
+
+    printf("\n-----------------------------------------\n");
 
 }
